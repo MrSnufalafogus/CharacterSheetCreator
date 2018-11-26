fd5ab3915969c0a7c3026355878bac052db77083
Jared Lehman <jrodpc15@hotmail.com>
2018-11-26 16:31:11 -0500

    Commit for services for the WebAPI changes.

---
+++ DDCharacterSheet/app/apps/characterSheet/services/CharacterSheetWebAPI/charactersService.js
@@ -0,0 +1,83 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("CharacterSheetApp")
+        .factory("charactersService", charactersService);
+
+    // Injected Services
+    charactersService.$inject = ["$http", "authenticationSettings", "authenticationHelper"];
+
+    function charactersService($http, authenticationSettings, authenticationHelper) {
+
+        ///////////////
+        // Functions //
+        ///////////////
+
+        var getCharacter = function (characterID) {
+            var url = authenticationSettings.serverPath + "api/Characters/" + characterID;
+
+            var req = {
+                method: "GET",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "Accept": "application/json",
+                    "AccessTokenID": authenticationHelper.getAccessTokenIDForAPI()
+                }
+            };
+
+            return $http(req);
+        };
+
+        var getCharactersByUserID = function (userID) {
+            var url = authenticationSettings.serverPath + "api/Characters/User";
+
+            var req = {
+                method: "GET",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "Accept": "application/json",
+                    "AccessTokenID": authenticationHelper.getAccessTokenIDForAPI()
+                },
+                params: {
+                    userID: userID
+                }
+            };
+
+            return $http(req);
+        };
+
+        var getCharacterByShareCode = function (shareCode) {
+            var url = authenticationSettings.serverPath + "api/Characters/ShareCode";
+
+            var req = {
+                method: "GET",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "Accept": "application/json",
+                    "AccessTokenID": authenticationHelper.getAccessTokenIDForAPI()
+                },
+                params: {
+                    shareCode: shareCode
+                }
+            };
+
+            return $http(req);
+        };
+
+        ////////////////////////
+        // Returned Functions //
+        ////////////////////////
+
+        return {
+            getCharacter: getCharacter,
+            getCharactersByUserID: getCharactersByUserID,
+            getCharacterByShareCode: getCharacterByShareCode
+        };
+
+    }
+
+})();
---
+++ DDCharacterSheet/app/apps/characterSheet/services/CharacterSheetWebAPI/racesService.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("CharacterSheetApp")
+        .factory("racesService", racesService);
+
+    // Injected Services
+    racesService.$inject = ["$http", "authenticationSettings", "authenticationHelper"];
+
+    function racesService($http, authenticationSettings, authenticationHelper) {
+
+        ///////////////
+        // Functions //
+        ///////////////
+
+        var getRaces = function () {
+            var url = authenticationSettings.serverPath + "api/Races";
+
+            var req = {
+                method: "GET",
+                url: url,
+                headers: {
+                    "Content-Type": "application/json; charset=utf-8",
+                    "Accept": "application/json",
+                    "AccessTokenID": authenticationHelper.getAccessTokenIDForAPI()
+                }
+            };
+
+            return $http(req);
+        };
+
+        ////////////////////////
+        // Returned Functions //
+        ////////////////////////
+
+        return {
+            getRaces: getRaces
+        };
+
+    }
+
+})();
