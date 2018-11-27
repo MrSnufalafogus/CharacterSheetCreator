2b21089de71a15eb7e7e1a253074d118e7ecc7a4
DESKTOP-JJNEESD\Logan <hannlg01@students.ipfw.edu>
2018-11-26 19:27:20 -0500

    WebAPI changes for ASIncrease

---
+++ CharacterSheetWebAPI.Contracts/ASIncrease.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetWebAPI.Contracts
+{
+    [DataContract(Namespace = "")]
+    public class ASIncrease
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public string Type { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Code { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int Amount { get; set; }
+    }
+}
