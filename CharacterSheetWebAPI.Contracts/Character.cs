43965f71b8cf5bb7bb54676f0fa70f7ddf8658f2
DESKTOP-JJNEESD\Logan <hannlg01@students.ipfw.edu>
2018-11-26 20:10:09 -0500

    API work for the characters

---
+++ CharacterSheetWebAPI.Contracts/Character.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetWebAPI.Contracts
+{
+    [DataContract(Namespace = "")]
+    public class Character
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public Guid CharacterID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public Guid UserID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string ShareCode { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int STR { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int DEX { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int CON { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int WIS { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int INT { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int CHA { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Alignment { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string ImageURL { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string EyeColor { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string HairColor { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Height { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Weight { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Biography { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int TraitID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public Trait Trait { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int ClassID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public Class Class { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public int RaceID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public Race Race { get; set; }
+    }
+}
