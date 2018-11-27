c20d785af2e4836dae1dd4b381fa3b7c024fe3b7
DESKTOP-JJNEESD\Logan <hannlg01@students.ipfw.edu>
2018-11-26 22:55:38 -0500

    API changes to classes and races

---
+++ CharacterSheetWebAPI.Contracts/Class.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetWebAPI.Contracts
+{
+    [DataContract(Namespace = "")]
+    public class Class
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int ClassID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string ClassImage { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Description { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string HitDie { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string PrimaryAbility { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string SavingThrows { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string ArmorProf { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string WeaponProf { get; set; }
+    }
+}
---
+++ CharacterSheetWebAPI.Contracts/Race.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterSheetWebAPI.Contracts
+{
+    [DataContract(Namespace = "")]
+    public class Race
+    {
+        [DataMember(EmitDefaultValue = false)]
+        public int RaceID { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Name { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string RaceImage { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Description { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public ASIncreases ASIncrease { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string AgeRange { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Size { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Speed { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Languages { get; set; }
+
+        [DataMember(EmitDefaultValue = false)]
+        public string Subtypes { get; set; }
+    }
+}
